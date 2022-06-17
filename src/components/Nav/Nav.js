170787bf984e7b73084fef1950b301cd9cbb8eab
Ruslan Sedziukh <ruslansedziukh@Katerynas-MacBook-Pro.local>
2022-06-17 13:23:37 +0300

    Added router. Made basic routing.

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <h1>Hello Nav!</h1>
+    );
+  }
+}
+
+export default Nav;
