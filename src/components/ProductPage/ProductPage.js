113353295d13b5b4921be61d8a6a5789bc0612c6
ruslan.sedziukh <>
2022-07-12 11:40:31 +0300

    Fetch product data from server.

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -6,10 +6,58 @@ import Attribute from "../Attribute/Attribute.js";
 import { withRouter } from "react-router-dom";
 
 class ProductPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getProduct = this.getProduct.bind(this);
+  }
+
+  async getProduct() {
+    let query1 = "query { category(input: {title: \"" + this.props.match.params.category + "\"}) { product (id: \"" + this.props.match.product + "\" ) { id name attributes { id name items { displayValue value id } } prices { currency { label symbol } amount } gallery } } }";
+    let query2 = "query { product (id: \"" + this.props.match.params.product + "\" ) { id name attributes { id name items { displayValue value id } } prices { currency { label symbol } amount } gallery } }"
+    console.log('>>>>>>> query2:');
+    console.log(query2);
+
+    try {
+      const response = await fetch('http://localhost:4000/', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          "query": query2
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        console.log('>>>>>>> data: ');
+        console.log(data);
+
+        const payload = {
+          category: this.props.match.params.category,
+          product: data.data.product
+        };
+
+        console.log('>>>>>>> payload: ');
+        console.log(payload);
+
+        // this.props.getProducts(payload);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
   render() {
-    // console.log(this.props.match.params.category);
-    // console.log(this.props.match.params.product);
-    console.log('ProductPage Render');
+    console.log(this.props.match.params.category);
+    console.log(this.props.match.params.product);
+    // console.log('ProductPage Render');
 
     return (
       <div className="product-page-container">
