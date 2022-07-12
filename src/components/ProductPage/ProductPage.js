fd3e326e0a3c4d636ba54f857bdc0e2891e2b70c
ruslan.sedziukh <>
2022-07-12 12:21:37 +0300

    Fetched data saves in component state. Add default attributes.

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -8,14 +8,21 @@ import { withRouter } from "react-router-dom";
 class ProductPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      product: {},
+      attributes: {}
+    };
+
     this.getProduct = this.getProduct.bind(this);
+    this.setDefaultAttributes = this.setDefaultAttributes.bind(this);
   }
 
   async getProduct() {
-    let query1 = "query { category(input: {title: \"" + this.props.match.params.category + "\"}) { product (id: \"" + this.props.match.product + "\" ) { id name attributes { id name items { displayValue value id } } prices { currency { label symbol } amount } gallery } } }";
-    let query2 = "query { product (id: \"" + this.props.match.params.product + "\" ) { id name attributes { id name items { displayValue value id } } prices { currency { label symbol } amount } gallery } }"
+    // let query1 = "query { category(input: {title: \"" + this.props.match.params.category + "\"}) { product (id: \"" + this.props.match.product + "\" ) { id name attributes { id name items { displayValue value id } } prices { currency { label symbol } amount } gallery } } }";
+    let query = "query { product (id: \"" + this.props.match.params.product + "\" ) { id name attributes { id name items { displayValue value id } } prices { currency { label symbol } amount } gallery } }"
     console.log('>>>>>>> query2:');
-    console.log(query2);
+    console.log(query);
 
     try {
       const response = await fetch('http://localhost:4000/', {
@@ -24,25 +31,19 @@ class ProductPage extends React.Component {
           'content-type': 'application/json'
         },
         body: JSON.stringify({
-          "query": query2
+          "query": query
         })
       });
 
       if (response.ok) {
         const data = await response.json();
 
-        console.log('>>>>>>> data: ');
-        console.log(data);
-
         const payload = {
           category: this.props.match.params.category,
           product: data.data.product
         };
 
-        console.log('>>>>>>> payload: ');
-        console.log(payload);
-
-        // this.props.getProducts(payload);
+        this.setState({ product: data.data.product });
       }
     }
     catch (error) {
@@ -50,14 +51,29 @@ class ProductPage extends React.Component {
     }
   }
 
-  componentDidMount() {
-    this.getProduct();
+  setDefaultAttributes() {
+    if(this.state.product.attributes) {
+      let attributes = {};
+      this.state.product.attributes.forEach(attribute => {
+        attributes[attribute.id] = { item: { id: attribute.items[0].id } };
+      });
+      this.setState({ attributes: attributes });
+    }
+  }
+
+  async componentDidMount() {
+    await this.getProduct();
+    this.setDefaultAttributes();
   }
 
   render() {
-    console.log(this.props.match.params.category);
-    console.log(this.props.match.params.product);
+    // console.log(this.props.match.params.category);
+    // console.log(this.props.match.params.product);
     // console.log('ProductPage Render');
+    // console.log('this.state.product');
+    // console.log(this.state.product);
+    // console.log('this.state.attributes');
+    // console.log(this.state.attributes);
 
     return (
       <div className="product-page-container">
